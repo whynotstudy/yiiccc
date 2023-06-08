1f073c82945a0930df9d8b21b39bb04b911217f3
master <974732752@qq.com>
2023-06-08 15:44:17 +0800

    AI注册

---
+++ views/ai/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = '这是AI写的一个注册mode';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">这是AI写的一个注册mode!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+    </div>
+</div>
